078433e886d6b0524965771770736e58a122a045
Divya Dadhich <divya.dadhich.s63@kalvium.community>
2025-02-12 16:06:25 +0530

    vite-2

---
+++ my-react-app/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Divya Dadhich</h1>
+      <p>Contact Details: dadhichdiv1504@gmail.com</p>
+    </div>
+  );
+};
+
+export default Home;
